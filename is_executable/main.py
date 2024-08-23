4f6a8e604834c586cbfa654c9faa671af6ae7875
Jens Kürten <jens.kuerten@contact-software.com>
2024-08-23 08:56:11 +0200

    add tests

--- is_executable/main.py
+++ is_executable/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Checks if the given files are marked as executable.
 You can pass multiple files, separated by a space.
@@ -6,33 +5,37 @@ You can pass multiple files, separated by a space.
 
 import subprocess  # nosec
 import sys
-
+import os.path
 
 def is_executable_with_git(filepath):
     """Check if a file is executable using Git."""
-    try:
-        # Run `git ls-files --stage <filepath>` to get the file mode
-        result = subprocess.run(  # nosec
-            ["git", "ls-files", "--stage", filepath],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-            check=True,
-        )
-        # The output looks like: "100755 <hash> 0 <filename>"
-        # The first part (100755) is the file mode. 100755 indicates an executable file.
-        if result.stdout.startswith("100755"):
-            return True
-    except subprocess.CalledProcessError:
-        pass
+
+    # Run `git ls-files --stage <filepath>` to get the file mode
+    result = subprocess.run(  # nosec
+        ["git", "ls-files", "--stage", filepath],
+        stdout=subprocess.PIPE,
+        text=True,  # output should be treated as string rather than bytes
+        check=True,  # non zero exit code should raise exception
+    )
+
+    # The output looks like: "100755 <hash> 0 <filename>"
+    # The first part (100755) is the file mode. 100755 indicates an executable file.
+    if result.stdout.startswith("100755"):
+        return True
+
+    if result.stdout == "":
+        # git ls-files returns nothing if the file could not be found or if it is not staged
+        print(f"{filepath} is not staged or does not exist")
     return False
 
 
 def main():
     non_executable_files = []
 
-    # Iterate over all files passed to the script
     for filepath in sys.argv[1:]:
+        if not os.path.isfile(filepath):
+            print(f"{filepath} does not exist!")
+            return 1  # Non-zero exit code to indicate failure
         if not is_executable_with_git(filepath):
             non_executable_files.append(filepath)
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 from unittest.mock import patch
-import sys
+import sys, os
 from is_executable.main import main
 
 
 class TestMain(TestCase):
+
     def test_main_executable(self):
+        # test that an executable file is detected correctly
         testargs = ["script.py", "./tests/fixtures/executable.sh"]
 
         with patch.object(sys, "argv", testargs):
@@ -13,13 +15,41 @@ class TestMain(TestCase):
             self.assertEqual(0, ret)
 
     def test_main_not_executable(self):
+        # test that a nonnexecutable file is detected
         testargs = ["script.py", "./tests/fixtures/not-executable.sh"]
         with patch.object(sys, "argv", testargs):
             ret = main()
             self.assertEqual(1, ret)
 
+    def test_unstaged_file(self):
+        # test that unstaged files return non-zero exit code
+        filepath = "./tests/fixtures/not-staged.sh"
+        with open(filepath, "w"):
+            pass
+        testargs = ["script.py", filepath]
+        with patch.object(sys, "argv", testargs):
+            ret = main()
+            self.assertEqual(1, ret)
+
+        os.remove(filepath)
+
     def test_main_not_exists(self):
+        # test that we return a non-zero code if a file doesn't exist
         testargs = ["script.py", "./tests/fixtures/not-exists"]
+        with patch.object(sys, "argv", testargs):
+            ret = main()
+            self.assertEqual(1, ret)
+
+    def test_pass_multiple_files_executable(self):
+        # test passing multiple executable files
+        testargs = ["script.py", "./tests/fixtures/executable.sh", "./tests/fixtures/executable2.sh"]
+        with patch.object(sys, "argv", testargs):
+            ret = main()
+            self.assertEqual(0, ret)
+
+    def test_pass_multiple_files_not_executable(self):
+        # test passing multiple files works and a non-executable is detected
+        testargs = ["script.py", "./tests/fixtures/executable.sh", "./tests/fixtures/not-executable.sh"]
         with patch.object(sys, "argv", testargs):
             ret = main()
             self.assertEqual(1, ret)
